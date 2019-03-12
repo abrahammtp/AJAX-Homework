8b6bc4cf76be574603f479833e2d15cf85e19a67
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-03-11 20:53:44 -0400

    Finished working on the style of the website, it is mobile responsive as well

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -7,78 +7,79 @@ $(document).ready(function () {
         var sport = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ujPjQgKwFJVF10SFXmts0jvOPRVeV0Cm&q=" + sport + "&limit=10&offset=0&rating=G&lang=en";
 
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-              }).then(function(response) {
-                console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-                var results = response.data;
+            var results = response.data;
 
-                for (var i = 0; i < results.length; i++) {
+            for (var i = 0; i < results.length; i++) {
 
-                    var sportDiv = $("<div>");
-        
-                    var p = $("<p>").text("Rating: " + results[i].rating);
-        
-                    var sportImage = $("<img>");
+                var sportDiv = $("<div>");
 
-                    sportImage.attr("src", results[i].images.fixed_height_still.url);
-                    sportImage.attr("data-still", results[i].images.fixed_height_still.url);
-                    sportImage.attr("data-animate", results[i].images.fixed_height.url);
-                    sportImage.attr("data-state", "still");
-                    sportImage.addClass("gif");
-                    
-        
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                p.addClass("rating");
 
-                    sportDiv.append(p);
-                    sportDiv.append(sportImage);
-        
-                    $("#sport-images").prepend(sportDiv);
-                    
-                  }
+                var sportImage = $("<img>");
 
-                  $(".gif").on("click", function() {
-                    var state = $(this).attr("data-state");
-                    if (state === "still") {
-                      $(this).attr("src", $(this).attr("data-animate"));
-                      $(this).attr("data-state", "animate");
-                    } else {
-                      $(this).attr("src", $(this).attr("data-still"));
-                      $(this).attr("data-state", "still");
-                    }
-                  });
+                sportImage.attr("src", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-still", results[i].images.fixed_height_still.url);
+                sportImage.attr("data-animate", results[i].images.fixed_height.url);
+                sportImage.attr("data-state", "still");
+                sportImage.addClass("gif");
 
-                
+
+
+                sportDiv.append(p);
+                sportDiv.append(sportImage);
+
+                $("#sport-images").prepend(sportDiv);
+
+            }
+
+            $(".gif").on("click", function () {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
             });
 
-            $("#sport-images").empty();
+
+        });
+
+        $("#sport-images").empty();
+    }
+
+    function renderButtons() {
+
+        $("#sport-buttons").empty();
+
+        for (var i = 0; i < sportsArray.length; i++) {
+
+            var a = $("<button>");
+            a.addClass("sport-buttons");
+            a.attr("data-name", sportsArray[i]);
+            a.text(sportsArray[i]);
+            $("#sport-buttons").append(a);
         }
+    }
 
-        function renderButtons() {
-
-            $("#sport-buttons").empty();
-    
-            for (var i = 0; i < sportsArray.length; i++) {
-    
-              var a = $("<button>");
-              a.addClass("sport-buttons");
-              a.attr("data-name", sportsArray[i]);
-              a.text(sportsArray[i]);
-              $("#sport-buttons").append(a);
-            }
-          }
-
-      $("#add-sport").on("click", function(event) {
+    $("#add-sport").on("click", function (event) {
         event.preventDefault();
         var sport = $("#sport-input").val().trim();
 
         sportsArray.push(sport);
 
         renderButtons();
-      });
+    });
 
-      $(document).on("click", ".sport-buttons", displaySport);
+    $(document).on("click", ".sport-buttons", displaySport);
 
-      renderButtons();
+    renderButtons();
 });
